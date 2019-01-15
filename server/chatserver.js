959569ad5c810a56565fa8b36a17c479594084e8
Akhil <akhilmadineni@outlook.com>
2019-01-15 01:59:00 -0800

    update

--- server/chatserver.js
+++ server/chatserver.js
@@ -14,13 +14,27 @@ app.use(express.static(publicPath));
 
 io.on(`connection`, (socket)=>{
   console.log('new user');
-
+   socket.emit('newMessage',{
+     from: 'Admin',
+     text: 'wELCOME',
+     createdAt: new Date().getTime()
+   });
+    socket.broadcast.emit('newMessage',{
+      from: 'Admin',
+      text: 'new user',
+      createdAt: new Date().getTime()
+    });
    socket.on('createMessage', (message)=>{
      console.log('createMessage',message);
-     io.emit('newMessage', {
-       from: message.from,
+     // io.emit('newMessage', {
+     //   from: message.from,
+     //   text: message.text,
+     //   createdAt: new Date().getTime()
+     // });
+     socket.broadcast.emit('newMessage',{
+       from : message.from,
        text: message.text,
-       createdAt: new Date().getTime()
+        createdAt: new Date().getTime()
      });
    });
 

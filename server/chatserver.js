730a08b0b40c1cfb2c2ad54cf0d872f01590e557
Akhil <akhilmadineni@outlook.com>
2019-01-15 16:51:29 -0800

    update

--- server/chatserver.js
+++ server/chatserver.js
@@ -4,7 +4,7 @@ const http = require('http');
 // const socketIO = require('socket.io');
 const socketIO = require('socket.io');
 
-const {generateMessage}= require('./Utils/message');
+const {generateMessage}= require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();

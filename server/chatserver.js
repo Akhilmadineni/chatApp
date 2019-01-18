a7d3c9c1b1185ced8f61a46cf1ded95ff28f22a7
Akhil <akhilmadineni@outlook.com>
2019-01-17 18:21:03 -0800

    Basic Functionalitis

--- server/chatserver.js
+++ server/chatserver.js
@@ -4,7 +4,7 @@ const http = require('http');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage}= require('./utils/message');
-const {isRealString}=require('/utils/validation');
+const {isRealString}=require('./utils/validation');
 const {Users}=require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
